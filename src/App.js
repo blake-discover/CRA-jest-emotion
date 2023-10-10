21b6562880319ed017dcf7269dc5f4918d09d283
Blake Netzeband <blakenetzeband@discover.com>
2023-10-10 12:00:50 -0600

    setup test. basic app structure. added emotion:

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+
+const StyledButton = styled.button`
+  background-color: aqua;
+  &:hover {
+    background-color: beige;
+  }
+`;
+
+const buttonCss = css`
+  background-color: aqua;
+  &:hover {
+    background-color: beige;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <StyledButton>Styled</StyledButton>
+      <button css={buttonCss}>CSS</button>
+    </section>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { screen, render } from "@testing-library/react";
+import each from "jest-each";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+
+describe("App", () => {
+  each([["Styled"], ["CSS"]]).test(
+    "It applies correct background-color with %s styling approach",
+    (query) => {
+      render(<App />);
+      const button = screen.queryByText(query);
+      expect(button).toBeInTheDocument();
+
+      expect(button).toHaveStyle({ "background-color": "aqua" });
+    }
+  );
 });
